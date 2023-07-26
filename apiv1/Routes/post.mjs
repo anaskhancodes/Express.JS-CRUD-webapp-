f9fb1d52ea7c2e526d83d2ea53a603fefc772c9c
M.Anas Khan <a4anas406@gmail.com>
2023-07-26 18:14:12 +0500

    ExpressJs Router webapp

--- apiv1/Routes/post.mjs
+++ apiv1/Routes/post.mjs
@@ -1,211 +1,98 @@
 
-// import express from 'express'
-// import { nanoid } from "nanoid"
-// let router = express.Router()
-
-// let posts = [
-//   {
-//     id: nanoid(),
-//     title: "abc post title",
-//     text: "some post text"
-//   }
-// ]
-
-
-// router.post('/post', (req, res, next) => {
-//   console.log("Post is Created => ", Date())
-//   // res.send("Post is Created => " + Date())
-
-// if (
-//   !req.body.title
-//   || !req.body.text
-// ) {
-//   res.status(403);
-//   res.send(`required parameters missing, 
-//   example request body:
-//   {
-//       title: "abc post title",
-//       text: "some post text"
-//   } `);
-//   return;
-// }
-
-// posts.unshift({
-//   id: nanoid(),
-//   title: req.body.title,
-//   text: req.body.text,
-// })
-
-// res.send('post created');
-// })
-
-
-
-// //Get    /posts/:userId
-// router.get('/posts/:userId', (req, res, next) => {
-//     console.log("Posts is Created", Date())
-//   })
-
-// router.get('/post/:postId', (req, res, next) => {
-
-//   if (req.params.postId) {
-//       res.status(403).send(`post id must be a valid number, no alphabet is allowed in post id`)
-//   }
-
-//   for (let i = 0; i < posts.length; i++) {
-//       if (posts[i].id === req.params.postId) {
-//           res.send(posts[i]);
-//           return;
-//       }
-//   }
-//   res.send('post not found with id ' + req.params.postId);
-// })
-
-
-
-
-
-
-
-
-
-
-
-// //Post    /post
-// router.get('/post', (req, res) => {
-//   console.log("Post is Created => ", Date())
-//   res.send("Post is Created => " + Date())
-// })
-
-
-// //Put    /post/:userId/:postId
-// router.put('/post/:userId/:postId', (req, res) => {
-//   console.log("Post is Put => ", Date())
-//   res.send("Post is Put => " + Date())
-// })
-
-// //Delete    /post/:userId/:postId
-// router.delete('/post/:userId/:postId', (req, res) => {
-//   console.log("This Post is Delete => ", Date())
-//   res.send("This Post is Delete => " + Date())
-// })
-
-// export default router
-
-
-
-
-
-import express from 'express';
-import { nanoid } from 'nanoid'
+import express from 'express'
+import { nanoid } from "nanoid"
 let router = express.Router()
 
-// not recommended at all - server should be stateless
 let posts = [
-    {
-        id: nanoid(),
-        title: "abc post title",
-        text: "some post text"
-    }
+  {
+    id: nanoid(),
+    title: "abc post title",
+    text: "some post text"
+  }
 ]
 
-// POST    /api/v1/post
+
 router.post('/post', (req, res, next) => {
-    console.log('this is signup!', new Date());
+  console.log("Post is Created => ", Date())
+  // res.send("Post is Created => " + Date())
 
-    if (
-        !req.body.title
-        || !req.body.text
-    ) {
-        res.status(403);
-        res.send(`required parameters missing, 
-        example request body:
-        {
-            title: "abc post title",
-            text: "some post text"
-        } `);
-        return;
-    }
+if (
+  !req.body.title
+  || !req.body.text
+) {
+  res.status(403);
+  res.send(`required parameters missing, 
+  example request body:
+  {
+      title: "abc post title",
+      text: "some post text"
+  } `);
+  return;
+}
 
-    posts.unshift({
-        id: nanoid(),
-        title: req.body.title,
-        text: req.body.text,
-    })
-
-    res.send('post created');
-})
-// GET     /api/v1/posts
-router.get('/posts', (req, res, next) => {
-    console.log('this is signup!', new Date());
-    res.send(posts);
+posts.unshift({
+  id: nanoid(),
+  title: req.body.title,
+  text: req.body.text,
 })
 
-// GET     /api/v1/post/:postId
+res.send('post created');
+})
+
+
+
+//Get    /posts/:userId
+router.get('/posts/:userId', (req, res, next) => {
+    console.log("Posts is Created", Date())
+  })
+
 router.get('/post/:postId', (req, res, next) => {
-    console.log('this is signup!', new Date());
 
-    if (req.params.postId) {
-        res.status(403).send(`post id must be a valid number, no alphabet is allowed in post id`)
-    }
+  if (req.params.postId) {
+      res.status(403).send(`post id must be a valid number, no alphabet is allowed in post id`)
+  }
 
-    for (let i = 0; i < posts.length; i++) {
-        if (posts[i].id === req.params.postId) {
-            res.send(posts[i]);
-            return;
-        }
-    }
-    res.send('post not found with id ' + req.params.postId);
+  for (let i = 0; i < posts.length; i++) {
+      if (posts[i].id === req.params.postId) {
+          res.send(posts[i]);
+          return;
+      }
+  }
+  res.send('post not found with id ' + req.params.postId);
 })
 
-// PUT     /api/v1/post/:userId/:postId
-// {
-//     title: "updated title",
-//     text: "updated text"
-// }
 
-router.put('/post/:postId', (req, res, next) => {
 
-    if (!req.params.postId
-        || !req.body.text
-        || !req.body.title) {
-        res.status(403).send(`example put body: 
-        PUT     /api/v1/post/:postId
-        {
-            title: "updated title",
-            text: "updated text"
-        }
-        `)
-    }
 
-    for (let i = 0; i < posts.length; i++) {
-        if (posts[i].id === req.params.postId) {
-            posts[i] = {
-                text: req.body.text,
-                title: req.body.title,
-            }
-            res.send('post updated with id ' + req.params.postId);
-            return;
-        }
-    }
-    res.send('post not found with id ' + req.params.postId);
+
+
+
+
+
+
+
+//Post    /post
+router.get('/post', (req, res) => {
+  console.log("Post is Created => ", Date())
+  res.send("Post is Created => " + Date())
 })
 
-// DELETE  /api/v1/post/:userId/:postId
-router.delete('/post/:postId', (req, res, next) => {
 
-    if (!req.params.postId) {
-        res.status(403).send(`post id must be a valid id`)
-    }
+//Put    /post/:userId/:postId
+router.put('/post/:userId/:postId', (req, res) => {
+  console.log("Post is Put => ", Date())
+  res.send("Post is Put => " + Date())
+})
 
-    for (let i = 0; i < posts.length; i++) {
-        if (posts[i].id === req.params.postId) {
-            posts.splice(i, 1)
-            res.send('post deleted with id ' + req.params.postId);
-            return;
-        }
-    }
-    res.send('post not found with id ' + req.params.postId);
+//Delete    /post/:userId/:postId
+router.delete('/post/:userId/:postId', (req, res) => {
+  console.log("This Post is Delete => ", Date())
+  res.send("This Post is Delete => " + Date())
 })
 
 export default router
+
+
+
+
+
