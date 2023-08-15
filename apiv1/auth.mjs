496f7b5f325653fa4cfc7ad27d556416270af0b9
M.Anas Khan <a4anas406@gmail.com>
2023-08-15 21:53:01 +0500

    JWT

--- apiv1/auth.mjs
+++ apiv1/auth.mjs
@@ -1,7 +1,7 @@
 import express from 'express'
 let router = express.Router()
 
-
+import authRouter from './auth.mjs'
 
 router.get('/login', (req, res) => {
   console.log("You're Login! => ", Date())
@@ -12,5 +12,7 @@ router.get('/SignUp', (req, res) => {
   console.log("You're SignUp => ", Date())
   res.send("You're SignUp => " + Date())
 })
+
+router.use(authRouter)
 
 export default router 
--- server.mjs
+++ server.mjs
@@ -1,19 +1,24 @@
 import express from 'express'
 import path from 'path';
+import apiv1 from './apiv1/auth.mjs'
+import apiv1Router from './apiv1/main.mjs'
+
 const __dirname = path.resolve();
 const app = express()
 app.use(express.json()); // body parser
-
 ////////////////////////////////////////////
 // app.use(cors())
 
-import apiv1Router from './apiv1/main.mjs'
+
+
+
+app.use("/api/v1", apiv1);
 
 app.use((req, res, next) => { // JWT
   let token = "valid";
   if (token === "valid"){
     next();
-
+    
   }else{
     res.send({message: "Invalid token"})
   }
